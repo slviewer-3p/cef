bac3767183960327e53d03da828552cdbfdb3ade
Callum Prentice <callum@mbp.local>
2021-06-11 09:51:49 -0700

    Update configuration to build CEF 91.0.4472.101

--- tools/build.bat
+++ tools/build.bat
@@ -28,8 +28,8 @@
 @set DEFAULT_ROOT_CODE_DIRECTORY=\code
 @set DEFAULT_BIT_WIDTH=64
 @set DEFAULT_PROPRIETARY_CODEC=1
-@set DEFAULT_BRANCH=4044
-@set DEFAULT_CEF_COMMIT_HASH="b223419"
+@set DEFAULT_BRANCH=4472
+@set DEFAULT_CEF_COMMIT_HASH="cf0c26a"
 @set DEFAULT_CEF_DISTRIB_SUBDIR="cef"
 
 @rem This "special" variable expands to the drive letter and path of the batch file it
